13a7dea5a668bb0366ee3187dab053838e427bbc
Sugarkhuu <sugarkhuul@gmail.com>
2023-09-19 01:06:52 +0200

    made flask simpler

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,67 +1,40 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import os
+# docker-compose build
+# docker-compose up -d postgres
+# docker-compose up -d flask_app
+
+
+import psycopg2
+from flask import Flask
 
 app = Flask(__name__)
 
-# Configure the PostgreSQL database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
+# Define a route that connects to PostgreSQL
+@app.route('/query_db')
+def query_db():
+    try:
+        connection = psycopg2.connect(
+            database="mydatabase",
+            user="myuser",
+            password="mypassword",
+            host="postgres",  # Use the Docker service name
+            port="5432"  # Use the PostgreSQL container port
+        )
+        cursor = connection.cursor()
 
-# Define a model for your data
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+        cursor.execute("SELECT * FROM your_table_name")
+        records = cursor.fetchall()
 
-@app.route('/items', methods=['GET'])
-def get_items():
-    items = Item.query.all()
-    return jsonify([item.name for item in items])
+        # Process records here
 
-@app.route('/items/<int:item_id>', methods=['GET'])
-def get_item(item_id):
-    item = Item.query.get(item_id)
-    if item:
-        return jsonify({'name': item.name})
-    else:
-        return jsonify({'message': 'Item not found'}), 404
+    except (Exception, psycopg2.Error) as error:
+        print("Error:", error)
 
-@app.route('/items', methods=['POST'])
-def create_item():
-    data = request.get_json()
-    name = data.get('name')
-    if name:
-        new_item = Item(name=name)
-        db.session.add(new_item)
-        db.session.commit()
-        return jsonify({'message': 'Item created successfully'}), 201
-    else:
-        return jsonify({'message': 'Invalid input data'}), 400
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
 
-@app.route('/items/<int:item_id>', methods=['PUT'])
-def update_item(item_id):
-    item = Item.query.get(item_id)
-    if item:
-        data = request.get_json()
-        new_name = data.get('name')
-        if new_name:
-            item.name = new_name
-            db.session.commit()
-            return jsonify({'message': 'Item updated successfully'})
-        else:
-            return jsonify({'message': 'Invalid input data'}), 400
-    else:
-        return jsonify({'message': 'Item not found'}), 404
-
-@app.route('/items/<int:item_id>', methods=['DELETE'])
-def delete_item(item_id):
-    item = Item.query.get(item_id)
-    if item:
-        db.session.delete(item)
-        db.session.commit()
-        return jsonify({'message': 'Item deleted successfully'})
-    else:
-        return jsonify({'message': 'Item not found'}), 404
+    return "Query result here"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
